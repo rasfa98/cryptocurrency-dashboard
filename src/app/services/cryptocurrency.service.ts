3179919898ed8b65e0a04fd6312291388ab9826a
Rasmus Falk <rf222fu@student.lnu.se>
2018-07-03 17:14:57 +0200

    Adding method for getting all cryptocurrencies

--- src/app/services/cryptocurrency.service.ts
+++ src/app/services/cryptocurrency.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptocurrencyService {
+  BASE_URL = 'https://api.coinmarketcap.com/v2/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCurrencies() {
+    return this.http.get(this.BASE_URL + 'listings');
+  }
 }
